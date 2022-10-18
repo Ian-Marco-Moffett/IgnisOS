c2c5861ccef145f51f95255c45f35c84ec36a83c
Ian Marco Moffett <spurious.vector@gmail.com>
2022-10-17 21:42:06 -0400

    Fix: Put log files in var/log.

--- share/docs/log.md
+++ share/docs/log.md
@@ -1,5 +1,5 @@
 ## IgnisOS kernel logs.
 
 If you are using qemu and you need a copy of the kernel logs
-you can check share/log/ignis.log for the log file after running
+you can check var/log/ignis.log for the log file after running
 qemu.
--- tools/run.sh
+++ tools/run.sh
@@ -1,3 +1,4 @@
-mkdir -p share/log/
-qemu-system-x86_64 --enable-kvm -cpu host -M q35 -m 3G -drive file=Ignis.iso -boot d -monitor stdio -smp 4 -rtc base=localtime -audiodev pa,id=audio0 -machine pcspk-audiodev=audio0 -serial file:share/log/ignis.log
+mkdir -p var/
+mkdir -p var/log/
+qemu-system-x86_64 --enable-kvm -cpu host -M q35 -m 3G -drive file=Ignis.iso -boot d -monitor stdio -smp 4 -rtc base=localtime -audiodev pa,id=audio0 -machine pcspk-audiodev=audio0 -serial file:var/log/ignis.log
 # qemu-system-x86_64 -d int -M q35 -m 3G -drive file=Ignis.iso -boot d -monitor stdio -serial telnet:localhost:4321,server,nowait -smp 4 -rtc base=localtime -audiodev pa,id=audio0 -machine pcspk-audiodev=audio0
