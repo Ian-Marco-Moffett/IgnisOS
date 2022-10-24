1e50f2c5defa46ba9738c86ce7417401fca3717e
ian <spurious.vector@gmail.com>
2022-10-23 23:40:46 -0400

    Feat: Startup services from service list.

--- usr/services/init/src/main.c
+++ usr/services/init/src/main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
 #include <sys/syscall.h>
+#include <services.cfg.h>
 
 void main(void) {
   puts(_CLEARSCREEN "-- IgnisInitD v0.0.1 --\n");
   puts("More is coming to the init daemon soon..\n");
-  syscall(0x1, "kesswm.sys");
+  
+  for (size_t i = 0; i < SIZEOF_ARRAY(services); ++i) {
+    syscall(0x1, services[i]);
+  }
+
   while (1);
 }
