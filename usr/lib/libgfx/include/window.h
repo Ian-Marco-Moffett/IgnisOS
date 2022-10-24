6abc4de65f2865611960de0dd3eae74d5ac7253a
ian <spurious.vector@gmail.com>
2022-10-24 15:38:24 -0400

    Fix: Return void.

--- usr/lib/libgfx/include/window.h
+++ usr/lib/libgfx/include/window.h
@@ -4,16 +4,7 @@
 #include <stdint.h>
 
 
-typedef struct {
-  uint32_t x;
-  uint32_t y;
-  uint32_t width;
-  uint32_t height;
-  uint32_t color;
-} window_t;
-
-
-window_t libgfx_open_window(uint32_t x, uint32_t y, uint32_t width, uint32_t height, uint32_t bg, uint32_t fg);
+void libgfx_open_window(uint32_t x, uint32_t y, uint32_t width, uint32_t height, uint32_t bg, uint32_t fg);
 
 
 #endif
--- usr/lib/libgfx/src/window.c
+++ usr/lib/libgfx/src/window.c
@@ -19,7 +19,7 @@ static void draw_window_bar(uint32_t win_x, uint32_t win_y, uint32_t width, uint
 }
 
 
-window_t libgfx_open_window(uint32_t x, uint32_t y, uint32_t width, uint32_t height, uint32_t bg, uint32_t fg) {
+void libgfx_open_window(uint32_t x, uint32_t y, uint32_t width, uint32_t height, uint32_t bg, uint32_t fg) {
   draw_square(x, y, width, height, bg);
   draw_window_bar(x, y, width, fg);
 }
