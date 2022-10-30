6530dcbfb6a3abb2cd8117d207ca250760d1d798
ian <spurious.vector@gmail.com>
2022-10-30 05:49:40 -0400

    Fix: Make window stay open.

--- usr/services/kesswm/src/main.c
+++ usr/services/kesswm/src/main.c
@@ -3,9 +3,8 @@
 #include <desktop.h>
 
 void main(void) {
-  while (1) {
-    kesswm_draw_desktop();
-    window_t welcome_window = kesswm_open_window(50, 50, 300, 300, "Welcome :-)");
-    kesswm_putstr(welcome_window, "Welcome to\nIgnisOS!");
-  }
+  kesswm_draw_desktop();
+  window_t welcome_window = kesswm_open_window(50, 50, 300, 300, "Welcome :-)");
+  kesswm_putstr(welcome_window, "Welcome to\nIgnisOS!");
+  while (1);
 }
